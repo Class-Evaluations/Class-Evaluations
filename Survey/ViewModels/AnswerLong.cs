d8e26a7a9208ba75053a7f7d6b3a5bf3cfc1d7b3
taylordl <donna.taylor@raleighnc.gov>
2012-05-31 11:40:16 -0400

    push for compile errors

---
+++ Survey/ViewModels/AnswerLong.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Survey.ViewModels
+{
+    public class AnswerLong
+    {
+        public string AnsLong { get; set; }
+    }
+}
